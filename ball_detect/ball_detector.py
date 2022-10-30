8d9b8cc1584f6a9771adda056af186f8aeb3108a
Ricky <ruei7916@gmail.com>
2022-10-30 17:24:56 +0800

    ball detect using inrange

--- ball_detect/ball_detector.py
+++ ball_detect/ball_detector.py
@@ -5,6 +5,10 @@ from rclpy.node import Node # Handles the creation of nodes
 from sensor_msgs.msg import Image # Image is the message type
 import cv2 # OpenCV library
 from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
+from collections import deque
+import numpy as np
+import imutils
+import time
 
  
 class ImageSubscriber(Node):
@@ -40,6 +44,51 @@ class ImageSubscriber(Node):
     # Convert ROS Image message to OpenCV image
     current_frame = self.br.imgmsg_to_cv2(data)
     
+    lower_orange = (15, 130, 200)
+    upper_orange = (25, 255, 255)
+    pts = deque(maxlen=args["buffer"])
+
+    current_frame = imutils.resize(current_frame, width=600)
+    blurred = cv2.GaussianBlur(current_frame, (11, 11), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+    mask = cv2.inRange(hsv, lower_orange, upper_orange)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    center = None
+    # only proceed if at least one contour was found
+    if len(cnts) > 0:
+      # find the largest contour in the mask, then use
+      # it to compute the minimum enclosing circle and
+      # centroid
+      c = max(cnts, key=cv2.contourArea)
+      ((x, y), radius) = cv2.minEnclosingCircle(c)
+      M = cv2.moments(c)
+      center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+      # only proceed if the radius meets a minimum size
+      if radius > 10:
+        # draw the circle and centroid on the frame,
+        # then update the list of tracked points
+        cv2.circle(current_frame, (int(x), int(y)), int(radius),
+                   (0, 255, 255), 2)
+        cv2.circle(current_frame, center, 5, (0, 0, 255), -1)
+    # update the points queue
+    pts.appendleft(center)
+    # loop over the set of tracked points
+    for i in range(1, len(pts)):
+      # if either of the tracked points are None, ignore
+      # them
+      if pts[i - 1] is None or pts[i] is None:
+        continue
+      # otherwise, compute the thickness of the line and
+      # draw the connecting lines
+      thickness = 64
+      cv2.line(current_frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+
     # Display image
     cv2.imshow("camera", current_frame)
     
